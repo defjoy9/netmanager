ef3b6d99ec0d67248626c61088d8bdb003718513
defjoy <defjoy9@users.noreply.github.com>
2025-09-18 13:08:16 +0200

    deleted unnecessary data

--- modify_sql.py
+++ modify_sql.py
@@ -4,43 +4,26 @@
 # 4. password
 # 5. producent
 
-
 # network_devices.db
-
 # -------------------------------------------
 
 import sqlite3
 
-conn = sqlite3.connect('network_devices1.2.db')
+conn = sqlite3.connect('network_devices.db')
 
 
 cur = conn.cursor()
-# cur.execute('''
-#     UPDATE devices
-#     SET enable = "1"
-#     WHERE ip_address = "192.168.137.16"
-# ''')
+
 cur.execute('''
     UPDATE devices
     SET ip_address="192.168.137.122"
     WHERE ip_address="192.168.137.76"
 ''')
-# cur.execute('''
-#     UPDATE devices
-#     SET password="dd"
-#     WHERE ip_address="10.22.2.1"
-# ''')
-# cur.execute('''
-#     UPDATE devices
-#     SET user_login="python"
-#     WHERE ip_address="192.168.137.40"
-# ''')
 
 conn.commit()
 
 cur.execute('SELECT * FROM devices')
 rows = cur.fetchall()
-
 
 for row in rows:
     print(row)
